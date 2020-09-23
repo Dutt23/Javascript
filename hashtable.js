2cdaa77412142d48d4b1e8e993d9578845d6dcc8
dutt23 <shatyaki_dutt@hotmail.com>
2020-09-23 23:39:26 +0530

    recurring problem

--- hashtable.js
+++ hashtable.js
@@ -1,6 +1,19 @@
+
+//  Add contains
 class HashTable {
 	constructor(size) {
 		this.data = new Array(size)
+		this.keys = []
+	}
+
+	keys() {
+		const keys = []
+		for (var val of this.data)
+			if (val)
+				for (let i = 0; i < val.length; i += 2) {
+					keys.push(val[i])
+				}
+		return keys;
 	}
 
 	set(key, value) {
@@ -11,7 +24,7 @@ class HashTable {
 		else {
 			const values = this.data[hash - 1]
 			var match = false;
-			for (let i = 0; i < values.length; i + 2) {
+			for (let i = 0; i < values.length; i += 2) {
 				if (values[i] === key) {
 					values[i + 1] = value
 					match = true;
@@ -30,7 +43,7 @@ class HashTable {
 		if (this.data[addr - 1]) {
 			for (let i = 0; i < this.data[addr - 1].length; i + 2) {
 				if (this.data[addr - 1][i] === key)
-					return this.data[addr- 1][i +1]
+					return this.data[addr - 1][i + 1]
 			}
 		}
 		return undefined
@@ -50,5 +63,75 @@ myHashTable.set('grapes', 100000)
 myHashTable.set('grapes', 999999)
 myHashTable.set('apples', 999999)
 myHashTable.set('apples', 100000)
-console.log(myHashTable.get('grapes'))
-console.log(myHashTable.data)
+myHashTable.set('oranges', 100000)
+// console.log(myHashTable.get('grapes'))
+// console.log(myHashTable.keys())
+// myHashTable.data.forEach(x => console.log(x))
+
+console.log(recurringItem([2, 5, 5, 1, 2, 3, 5, 1, 2, 4]))
+console.log(recurringItem2([2, 5, 5, 1, 2, 3, 5, 1, 2, 4]))
+console.log(recurringItem3([2, 5, 5, 1, 2, 3, 5, 1, 2, 4]))
+
+// [2,5,5,2] will return 5
+function recurringItem(array = []) {
+	if (array.length === 0)
+		return undefined
+	const myObj = {}
+	for (item of array) {
+		if (!myObj[item])
+			myObj[item] = true
+		else return item
+	}
+}
+
+// [2,5,5,2] will return 2
+function recurringItem2(array = []) {
+	const obj = new Map()
+	for (item of array) {
+		if (obj.has(item))
+			obj.set(item, obj.get(item) + 1)
+		else
+			obj.set(item, 1)
+	}
+
+	for (key of obj.keys()) {
+		if (obj.get(key) > 1)
+			return key
+	}
+}
+
+// [2,5,5,2] will return 2
+// Without in built map
+function recurringItem3(array = []) {
+	const objArray = []
+	for (item of array) {
+		if(objArray.length === 0)
+		objArray.push({
+			item,
+			count : 1
+		})
+		else 
+		{
+			var index = objArray.findIndex(i => i["item"]===item)
+			if(index > -1)
+			{
+				var targetItem = objArray[index];
+				targetItem["count"] = targetItem["count"]  + 1
+				objArray[index] = targetItem
+			}
+			else {
+				objArray.push({
+					item,
+					count : 1
+				})
+			}
+			
+		}
+	}
+	for( obj of objArray)
+	{
+		if(obj["count"] > 1)
+		return obj["item"]
+	}
+}
+
