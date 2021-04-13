9ec4e3b8df29ac8e5fce51e880b7e2c31078f44d
dutt_23 <shatyaki_dutt@hotmail.com>
2021-04-13 20:14:27 +0530

    types of graphs

---
+++ graph.js
@@ -0,0 +1,14 @@
+// Edge list 
+const edgeList = [[0, 2], [2, 3], [2,1], [1, 3]]
+
+// Adjacent list 
+// Index of the element is the node
+const adjacentList =[[2], [2,3], [0, 1, 3], [1, 2]]
+
+// Adjaceny matric
+const matrix = [
+	[0, 1, 0, 0],
+	[0, 0, 1, 1],
+	[1, 0, 1, 1],
+	[0, 1, 1, 0]
+]
