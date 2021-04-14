53d5a8c3ea3efde52a4f055272ece937338e37f0
dutt_23 <shatyaki_dutt@hotmail.com>
2021-04-14 16:43:07 +0530

    factorial recur

--- algo/recursive.js
+++ algo/recursive.js
@@ -3,11 +3,37 @@
 //  2. Recursive case
 //  3. Closer and return when needed. Usually has 2 returns. One base case and normal func return
 let counter = 0;
-function inception(){
-	if(counter > 3)
-	return "Done"
+function inception() {
+	if (counter > 3)
+		return "Done"
 	counter++;
 	return inception();
 }
 
 console.log(inception())
+
+
+// O(n)
+function factorialNormal(num = 1) {
+	let sum = 1;
+	for (let i = 1; i <= num; i++) {
+		sum *= i
+	}
+
+	return sum
+}
+
+// O(n), Calling the functions same numbeer of times as length of num.
+function factorialRecursion(num)
+{
+	let sum = 1;
+	// Base case
+	if(num == 1)
+	return num
+
+	sum *= (num * factorialRecursion(num -1 ))
+
+	return sum;
+}
+console.log(factorialNormal(5))
+console.log(factorialRecursion(4))
