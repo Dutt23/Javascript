6d267fd48a2a234bce83b36aa5d83a0c2a45bbb8
dutt_23 <shatyaki_dutt@hotmail.com>
2021-04-17 15:22:55 +0530

    change

--- algo/sorting.js
+++ algo/sorting.js
@@ -1,13 +1,15 @@
 const { ifError } = require("assert")
 
 
-function bubbleSort(arr = []) {
-	for (let i = 0; i < arr.length - 1; i++) {
+function bubbleSort(arr=[]){
+	for(let i = 0 ; i < arr.length -1 ; i ++){
 		// Largest element at the back so can ignore the last elements
-		for (let j = 0; j < arr.length - i - 1; j++) {
-			if (arr[j] > arr[j + 1]) {
-				let temp = arr[j + 1]
-				arr[j + 1] = arr[j]
+		for(let j = 0 ; j <arr.length -i-1 ; j ++)
+		{
+			if(arr[j] > arr[j+1])
+			{
+				let temp = arr[j+1]
+				arr[j+1] = arr[j]
 				arr[j] = temp;
 			}
 		}
@@ -15,16 +17,17 @@ function bubbleSort(arr = []) {
 	console.log(arr)
 }
 
-function selectionSort(arr = []) {
-	for (let i = 0; i < arr.length; i++) {
+function selectionSort(arr=[]){
+	for(let i = 0 ; i < arr.length; i++){
 		let index = i;
-		for (let j = i + 1; j < arr.length; j++) {
-			if (arr[index] > arr[j]) {
+		for(let j = i + 1; j <arr.length;j ++){
+			if(arr[index] > arr[j])
+			{
 				index = j;
-			}
+			} 
 		}
-		let temp = arr[index];
-		arr[index] = arr[i]
+		let temp = arr[index]
+		arr[index] = arr[i];
 		arr[i] = temp;
 	}
 	console.log(arr);
