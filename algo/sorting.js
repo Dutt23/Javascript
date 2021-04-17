d1e829b4406d668997e8ea15974117a6e0f6ce69
dutt_23 <shatyaki_dutt@hotmail.com>
2021-04-18 01:14:18 +0530

    merge sort

--- algo/sorting.js
+++ algo/sorting.js
@@ -1,4 +1,5 @@
-const { ifError } = require("assert")
+const { ifError } = require("assert");
+const { exit } = require("process");
 
 
 function bubbleSort(arr = []) {
@@ -15,21 +16,18 @@ function bubbleSort(arr = []) {
 	console.log(arr)
 }
 
-function selectionSort(arr=[]){
-	for(let i = 0 ; i < arr.length ; i ++)
-	{
+function selectionSort(arr = []) {
+	for (let i = 0; i < arr.length; i++) {
 		let min_index = i;
-		for(let j = i +1 ; j < arr.length; j ++)
-		{
-			if(arr[min_index] > arr[j])
-			{
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[min_index] > arr[j]) {
 				min_index = j;
 			}
-			
+
 		}
 		let temp = arr[i];
-			arr[i] = arr[min_index];
-			arr[min_index] = temp;
+		arr[i] = arr[min_index];
+		arr[min_index] = temp;
 	}
 	console.log(arr)
 }
@@ -48,5 +46,59 @@ function insertionSort(arr = []) {
 	}
 	console.log(arr)
 }
+
+function sort(arr, start, end, middle){
+	let array1Length = middle - start + 1;
+	let array2Length = end - middle;
+
+	let array1 = [];
+	let array2 = [];
+	for(let i = 0 ; i < array1Length ; ++i)
+	array1[i] = arr[start +i];
+	for(let j = 0 ; j < array2Length; ++j)
+	array2[j] = arr[middle + j + 1]
+
+	let i = 0;
+	let j = 0;
+	let mergedIndex = start;
+
+	while (i < array1Length && j < array2Length) {
+		if(array1[i] <= array2[j])
+		{
+			arr[mergedIndex] = array1[i];
+			i++;
+		}
+		else {
+			arr[mergedIndex] = array2[j];
+			j++;
+		}
+		mergedIndex ++;
+	}
+
+	while ( i < array1Length ) {
+		arr[mergedIndex] = array1[i];
+		i++;
+		mergedIndex ++;
+	}
+	while ( j < array2Length ) {
+		arr[mergedIndex] = array2[j];
+	   j++;
+		mergedIndex ++;
+	}
+	
+}
+
+function mergeSort(arr, start, end){
+	if(start < end){
+		// Find half of array and add to start
+		let middle = Math.floor(start + ((end -start)/ 2))
+
+		mergeSort(arr, start, middle)
+		mergeSort(arr, middle + 1, end)
+
+		sort(arr, start, end, middle)
+	}
+}
 const array = [64, 34, 25, 12, 22, 11, 90, 10, 18]
-selectionSort(array)
+mergeSort(array, 0, array.length - 1)
+console.log(array)
