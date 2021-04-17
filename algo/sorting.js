328b9883b78b65b4ea4d36e73be6dd27f2cda3a6
dutt_23 <shatyaki_dutt@hotmail.com>
2021-04-17 21:54:51 +0530

    insertion sort

--- algo/sorting.js
+++ algo/sorting.js
@@ -1,15 +1,13 @@
 const { ifError } = require("assert")
 
 
-function bubbleSort(arr=[]){
-	for(let i = 0 ; i < arr.length -1 ; i ++){
+function bubbleSort(arr = []) {
+	for (let i = 0; i < arr.length - 1; i++) {
 		// Largest element at the back so can ignore the last elements
-		for(let j = 0 ; j <arr.length -i-1 ; j ++)
-		{
-			if(arr[j] > arr[j+1])
-			{
-				let temp = arr[j+1]
-				arr[j+1] = arr[j]
+		for (let j = 0; j < arr.length - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				let temp = arr[j + 1]
+				arr[j + 1] = arr[j]
 				arr[j] = temp;
 			}
 		}
@@ -17,14 +15,13 @@ function bubbleSort(arr=[]){
 	console.log(arr)
 }
 
-function selectionSort(arr=[]){
-	for(let i = 0 ; i < arr.length; i++){
+function selectionSort(arr = []) {
+	for (let i = 0; i < arr.length; i++) {
 		let index = i;
-		for(let j = i + 1; j <arr.length;j ++){
-			if(arr[index] > arr[j])
-			{
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[index] > arr[j]) {
 				index = j;
-			} 
+			}
 		}
 		let temp = arr[index]
 		arr[index] = arr[i];
@@ -32,5 +29,18 @@ function selectionSort(arr=[]){
 	}
 	console.log(arr);
 }
-const array = [64, 34, 25, 12, 22, 11, 90, 10]
-selectionSort(array)
+
+function insertionSort(arr = []) {
+	for (let i = 0; i < arr.length; i++) {
+		let value = arr[i];
+		let j = i - 1;
+		while (j >= 0 && arr[j] > value) {
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = value
+	}
+	console.log(arr)
+}
+const array = [64, 34, 25, 12, 22, 11, 90, 10, 18]
+insertionSort(array)
