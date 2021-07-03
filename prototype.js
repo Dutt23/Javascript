c6f0bca864dc7bf21780c14782b7fc1c95223b70
dutt_23 <shatyaki_dutt@hotmail.com>
2021-07-03 13:44:58 +0530

    prototype examples

--- prototype.js
+++ prototype.js
@@ -30,6 +30,34 @@ for(let prop in lizard){
   console.log(prop)
 }
 
+// Using normal function instead of arrow because of this and context
+Date.prototype.lastYear = function(){
+  if(!this)
+  return new Date().getFullYear() - 1
+  else 
+  return this.getFullYear() - 1
+} 
+
+ console.log(new Date('1998-10-10').lastYear())
+
+
+ Array.prototype.map = function(){
+   const resultArray = [];
+   if(arguments && typeof arguments[0] === "function"){
+     const func = arguments[0];
+     for(let i of this){
+      resultArray.push(func(i))
+     }
+   }
+   else {
+    for(let i of this){
+      resultArray.push( i +"*")
+     }
+   }
+   return resultArray;
+ }
+ console.log([1,2,3].map())
+
 // Object is a function, which creates the object prototype
 // Object.proptotype is an object
 // Only functions have the prototype property
