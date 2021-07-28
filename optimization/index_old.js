350c615610a693b09ab3401c784714aacee9270f
Dutt23 <shatyaki_dutt@hotmail.com>
2021-07-28 15:51:43 +0530

    manual cluster setup

--- optimization/index_old.js
+++ optimization/index_old.js
@@ -1,10 +1,21 @@
 const cluster = require('cluster')
+const totalCPUs = require('os').cpus().length;
 process.env.UV_THREADPOOL_SIZE = 2
 
 if (cluster.isMaster) {
   // Cause index.js to be executed again but in child mode
-  cluster.fork();
-  cluster.fork();
+  console.log(`Number of CPUs is ${totalCPUs}`);
+  console.log(`Master ${process.pid} is running`);
+
+  for (let i = 0; i < totalCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    console.log("Let's fork another worker!");
+    cluster.fork();
+  });
 }
 else {
 
